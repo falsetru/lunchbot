6bcc0985c2ca4584d4a923cd641310802627e616
Jeong-Min Lee <jeong-min.lee@yogiyo.co.kr>
2014-11-03 00:02:43 +0900

    Test for !fin, !salt.

--- tests/test_order_commands.py
+++ tests/test_order_commands.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import json
+
 import mock
 import pytest
 
@@ -16,15 +18,34 @@ def menus(request):
         u'고기고기도시락': 3000,
         u'해피박스': 1000,
     }
+    mocks = {}
 
-    def get(self, name):
+    def get(name):
         price = tbl.get(name)
         if not price:
             return
         return name, price
-    patcher = mock.patch('storage.Menu.get', get)
+
+    def get_last_order(handle, idx):
+        if idx == 0:
+            return json.dumps({u'고기고기도시락': 1})
+        else:
+            return json.dumps({u'해피박스': 2})
+
+    patcher = mock.patch('storage.Menu.get', side_effect=get)
     patcher.start()
     request.addfinalizer(patcher.stop)
+
+    patcher = mock.patch('storage.OrderRecord.add')
+    mocks['add'] = patcher.start()
+    request.addfinalizer(patcher.stop)
+
+    patcher = mock.patch(
+        'storage.OrderRecord.get_last_order',
+        side_effect=get_last_order)
+    mocks['get_last_order'] = patcher.start()
+    request.addfinalizer(patcher.stop)
+    return mocks
 
 
 def test_orders_response_with_ordered_menu_summary(cmd, menus):
@@ -90,3 +111,24 @@ def test_sum(cmd, menus):
     assert u'a-fullname (a): 고기고기도시락 x 1 = 3,000' in got
     assert u'b-fullname (b): 고기고기도시락 x 1 = 3,000' in got
     assert u'Total: 6,000' in got
+
+
+def test_fin(cmd, menus):
+    in_(cmd, u'고기고기도시락')
+    in_(cmd, u'고기고기도시락 x 2', FromHandle='b')
+    in_(cmd, u'!fin')
+    assert menus['add'].call_count == 2
+    menus['add'].assert_has_calls([
+        mock.call('a', 'a-fullname', {u'고기고기도시락': 1}, 3000, mock.ANY),
+        mock.call('b', 'b-fullname', {u'고기고기도시락': 2}, 6000, mock.ANY)
+    ])
+
+
+def test_salt(cmd, menus):
+    in_(cmd, u'!salt')
+    assert cmd.orders['a'].menus == {u'고기고기도시락': 1}
+    in_(cmd, u'!salt 0')
+    assert cmd.orders['a'].menus == {u'고기고기도시락': 1}
+    in_(cmd, u'!salt 1')
+    assert cmd.orders['a'].menus == {u'해피박스': 2}
+    assert menus['get_last_order'].call_count == 3
